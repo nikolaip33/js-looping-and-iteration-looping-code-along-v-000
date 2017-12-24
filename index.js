c9218c1d339f9fadb647d13f071f01d75eca644a
Nikolai Pawlowski <nikolaip@gmail.com>
2017-12-24 15:37:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function printBadges(names) {
   for(let i = 1; i <= names.length; i++){
-    console.log(`Welcome ${names[i]}! You are employee #${i}.`);
+    console.log(`Welcome ${names[i-1]}! You are employee #${i}.`);
   }
 }
